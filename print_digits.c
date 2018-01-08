386fc4c1e03ec4e6dd781d63571fba1b3e7591a2
Iuliia MELNYCHENKO <imelnych@e2r6p7.unit.ua>
2018-01-08 17:39:40 +0200

    feat: handle + -

--- print_digits.c
+++ print_digits.c
@@ -6,11 +6,32 @@
 /*   By: imelnych <imelnych@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 15:43:07 by imelnych          #+#    #+#             */
-/*   Updated: 2018/01/08 16:04:30 by imelnych         ###   ########.fr       */
+/*   Updated: 2018/01/08 17:37:44 by imelnych         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printflib.h"
+
+void	print_spaces(list_spec cr, int i)
+{
+	while (i > (cr.precs > ft_numlen(cr.buf) ? cr.precs : ft_numlen(cr.buf)))
+		{
+			write(1, " ", 1);
+			i--;
+		}
+}
+
+void	print_zeros(list_spec cr)
+{
+	int i;
+
+	i = 0;
+	while (i < (cr.precs - ft_numlen(cr.buf)))
+	{
+		write(1, "0", 1);
+		i++;
+	}
+}
 
 void	print_digits(va_list *args, list_spec cr)
 {
@@ -19,21 +40,28 @@ void	print_digits(va_list *args, list_spec cr)
 	cr.buf = (va_arg(*args, int));
 	if (cr.precs && cr.width)
 	{
-		i = cr.width;
-		while (i > (cr.precs > ft_numlen(cr.buf) ? cr.precs : ft_numlen(cr.buf)))
+		if (cr.flag[0] == 0)
 		{
-			write(1, " ", 1);
-			i--;
-		}
-		if (cr.precs > ft_numlen(cr.buf))
-		{
-			i = 0;
-			while (i < (cr.precs - ft_numlen(cr.buf)))
+			cr.flag[1] == 1 ? (i = cr.width - 1) : (i = cr.width);
+			print_spaces(cr, i);
+			if (cr.precs > ft_numlen(cr.buf))
 			{
-				write(1, "0", 1);
-				i++;
+				print_zeros(cr);
 			}
 		}
+		else
+		{
+			cr.flag[1] == 1 ? (i = cr.width - 1) : (i = cr.width);
+			if (cr.precs > ft_numlen(cr.buf))
+			{
+				print_zeros(cr);
+				ft_putnbr(cr.buf);
+			}
+			print_spaces(cr, i);
+		}
+
+		
+		cr.flag[1] == 1 ? write(1, "+", 1) : i;
 		ft_putnbr(cr.buf);
 	}
 	if (cr.precs && !cr.width)
@@ -46,7 +74,7 @@ void	print_digits(va_list *args, list_spec cr)
 				write(1, "0", 1);
 				i++;
 			}
+			ft_putnbr(cr.buf);
 		}
-		ft_putnbr(cr.buf);
 	}
 }
